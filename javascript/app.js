f12087e2271ff78b5a69e27324192c36a1e87f48
Aayushi Singh <aayushi.singh067@gmail.com>
2024-07-16 19:17:02 +0530

    updated custom hover properties

--- javascript/app.js
+++ javascript/app.js
@@ -21,18 +21,8 @@ const hoverSpaces = document.querySelectorAll('.left, li, a, .text-body, .header
 
 const updateCursorPosition = (event) => {
     customCursor.style.top = `${event.clientY}px`;
-    customCursor.style.left = `${event.clientX}px`
-}
-
-// window.addEventListener('mousemove', (event) => {
-//     updateCursorPosition(event)
-
-//     if(hoverSpace.matches(':hover')){
-//         customCursor.classList.add('zoom') 
-//     } else{
-//         customCursor.classList.remove('zoom')
-//     }
-// })
+    customCursor.style.left = `${event.clientX}px`;
+};
 
 window.addEventListener('mousemove', (event) => {
     updateCursorPosition(event);
